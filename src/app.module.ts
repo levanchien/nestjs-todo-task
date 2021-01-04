80d823a802065787d42b26ff96855b7e76be0d58
Le Van Chien <levanchien.95.cit@gmail.com>
2021-01-04 11:06:57 +0700

    Install db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
+import { SequelizeModule } from '@nestjs/sequelize';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    SequelizeModule.forRoot({
+      dialect: 'mssql',
+      host: 'localhost',
+      port: 3306,
+      username: 'sa',
+      password: '123',
+      database: 'todo_task',
+      define: {
+        timestamps: false,
+        underscored: true
+      },
+      models: [
+
+      ]
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
