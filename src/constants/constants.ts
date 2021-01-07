d120d4ab36ecd8e7a2f71cb1cd1e2b03f321f6ac
Le Van Chien <levanchien.95.cit@gmail.com>
2021-01-07 13:47:38 +0700

    Refactor

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -2,7 +2,3 @@ export const APP_SECRET = 'the fox jump over lazy dog';
 export const JWT = {
     secret: 'JD3T2-QH36R-X7W2W-7R3XT-DVRPQ'
 }
-
-export const BEARER_OPTS = {
-    clientID: 'JD3T2-QH36R-X7W2W-7R3XT-DVRPQ'
-}
